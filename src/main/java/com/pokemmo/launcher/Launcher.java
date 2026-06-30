28736d3f552121642ffb194da63c33170c45ee3c
Desu <desu@pokemmo.eu>
2026-06-30 21:58:47 +0000

    chore: update release to 4.0a

--- src/main/java/com/pokemmo/launcher/Launcher.java
+++ src/main/java/com/pokemmo/launcher/Launcher.java
@@ -66,7 +66,7 @@ import com.pokemmo.launcher.util.WindowsUtil;
 public class Launcher
 {
 	public static final int INSTALLER_VERSION = 60;
-	public static final String INSTALLER_VERSION_CODE = "4.0a-beta5";
+	public static final String INSTALLER_VERSION_CODE = "4.0a";
 
 	public static final int EXIT_CODE_SUCCESS = 0;
 	public static final int EXIT_CODE_NETWORK_FAILURE = 1;
