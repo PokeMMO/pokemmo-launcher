b4d58db7d10a555bd50860111254e341f37c51f5
Desu <desu@pokemmo.eu>
2026-07-26 23:37:34 -0400

    fix: better theme aware support for details messages

--- src/swt/java/com/pokemmo/launcher/ui/swt/LocaleAwareTextArea.java
+++ src/swt/java/com/pokemmo/launcher/ui/swt/LocaleAwareTextArea.java
@@ -8,30 +8,35 @@ import com.pokemmo.launcher.ui.shared.LocaleAwareElementManager;
 import com.pokemmo.launcher.ui.shared.LocaleAwareInterface;
 import com.pokemmo.launcher.ui.shared.LocaleAwareStringBundle;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Text;
 
 /**
- * SWT {@link Text} (multi-line, wrapped, scrollable) that supports locale-aware
- * content re-resolution.
+ * SWT {@link StyledText} (multi-line, wrapped, scrollable) that supports locale-aware
+ * content re-resolution and properly respects system dark mode colors on macOS.
  *
  * @author Kyu
  */
-public class LocaleAwareTextArea extends Text implements LocaleAwareInterface
+public class LocaleAwareTextArea extends StyledText implements LocaleAwareInterface
 {
     private final List<LocaleAwareStringBundle> appendedLines = new ArrayList<>();
 
     public LocaleAwareTextArea(Composite parent, int style)
     {
-        super(parent, style | SWT.MULTI | SWT.WRAP | SWT.V_SCROLL);
+        super(parent, style | SWT.V_SCROLL);
+        setWordWrap(true);
         setEditable(false);
+        setLeftMargin(6);
+        setRightMargin(6);
+        setTopMargin(6);
+        setBottomMargin(6);
         LocaleAwareElementManager.instance.addElement(this);
     }
 
     @Override
     protected void checkSubclass()
     {
-        // Allow subclassing of SWT Text
+        // Allow subclassing of SWT StyledText
     }
 
     @Override
@@ -64,6 +69,9 @@ public class LocaleAwareTextArea extends Text implements LocaleAwareInterface
         String resolved = Config.getString(str, params);
         appendedLines.add(bundle);
         super.append(resolved);
+
+        // Auto-scroll to bottom
+        setTopIndex(getLineCount() - 1);
     }
 
     @Override
